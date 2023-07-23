b46ed7f621027bec1ee72bde45b48d00ed7c6669
arnabmitra <arnabmitra.mitra@gmail.com>
2023-07-23 16:58:50 -0600

    adding base.

---
+++ src/lib/TodoList.svelte
@@ -0,0 +1,25 @@
+<script>
+    import Todo from './Todo.svelte';
+    let newTodo = '';
+    let todos = [];
+
+    function addTodo() {
+        if (newTodo !== '') {
+            todos = [...todos, newTodo];
+            newTodo = '';
+        }
+    }
+
+    function removeTodo(index) {
+        todos = todos.filter((_, i) => i !== index);
+    }
+</script>
+
+<div class="todo-list">
+    <input bind:value={newTodo} placeholder="New todo" />
+    <button on:click={addTodo}>Add</button>
+
+    {#each todos as todo, index (index)}
+        <Todo {todo} remove={() => removeTodo(index)} />
+    {/each}
+</div>
---
+++ src/lib/todo.svelte
@@ -0,0 +1,17 @@
+<script>
+    export let todo;
+    export let remove;
+</script>
+
+<div class="todo-item">
+    <p>{todo}</p>
+    <button on:click={remove}>Remove</button>
+</div>
+
+<style>
+    .todo-item {
+        display: flex;
+        justify-content: space-between;
+        margin-bottom: 1em;
+    }
+</style>
